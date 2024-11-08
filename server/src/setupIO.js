50cf0bac0ef51fc70bca1194b01d08320c2d817a
Kunal19880711 <kunal21634267+github@gmail.com>
2024-11-08 16:03:39 +0530

    fix + chore:  fixing leave room + adding comments

--- server/src/setupIO.js
+++ server/src/setupIO.js
@@ -16,15 +16,21 @@ export default function setupIO(server) {
 
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
+    // 1. send reset event to socket
     socket.emit("reset");
 
+    // 2. initialize socket's data in socketMap
     socketMap.set(socket.id, {
       name: null,
       roomSet: new Set(),
     });
 
+    
     socket.on("setIdentity", (name) => {
+      // 1. set socket's name in socketMap
       socketMap.get(socket.id).name = name;
+
+      // 2. notify socket with confirmIdentity event
       socket.emit("confirmIdentity", {
         name,
         rooms: [...roomMap.keys()],
@@ -32,57 +38,101 @@ export default function setupIO(server) {
     });
 
     socket.on("createRoom", (room) => {
+      // 1. add room to roomMap
       roomMap.set(room, new Set([socket.id]));
+
+      // 2. add room to socket's roomSet
       socketMap.get(socket.id).roomSet.add(room);
+
+      // 3. notify all sockets (including the one that created the room)
       io.emit("roomAdded", room);
 
+      // 4. join the socket to the room
       socket.join(room);
+
+      // 5. notify the socket that joined the room
       const members = getMemberNamesOfRoom(room);
       socket.emit("roomJoined", { room, members });
     });
 
     socket.on("deleteRoom", (room) => {
+      // 1. get the set of sockets in the room
       const members = roomMap.get(room);
-      for (let member of members) {
-        socketMap.get(member).roomSet.delete(room);
+
+      // 2. remove the room from each socket's roomSet
+      for (let memberSocket of members) {
+        socketMap.get(memberSocket).roomSet.delete(room);
       }
+
+      // 3. every member should leave the room
+      for (let memberSocket of members) {
+        memberSocket.leave(room);
+      }
+
+      // 4. notify all sockets that the room was deleted
       io.emit("roomDeleted", room);
     });
 
     socket.on("joinRoom", (room) => {
-      const { name } = socketMap.get(socket.id);
+      // 1. add room to socket's roomSet
       socketMap.get(socket.id).roomSet.add(room);
+
+      // 2. add socket to room's set of sockets
       roomMap.get(room).add(socket.id);
 
+      // 3. get the set of names in the room
       const members = getMemberNamesOfRoom(room);
 
+      // 4. join the socket to the room
       socket.join(room);
+
+      // 5. notify the socket that joined the room
       socket.emit("roomJoined", { room, members });
+
+      // 6. notify all other sockets in the room that another user joined
+      const { name } = socketMap.get(socket.id);
       io.to(room).emit("userJoined", { room, name });
       console.log("send used joined", { room, name });
     });
 
     socket.on("leaveRoom", (room) => {
       const { name } = socketMap.get(socket.id);
+
+      // Step 1: Remove the room from the user's roomSet
       socketMap.get(socket.id).roomSet.delete(room);
+
+      // Step 2: Remove the user from the room's set of sockets
       roomMap.get(room).delete(socket.id);
 
-      socket.leave(room);
-
+      // Step 3: Notify all users in the room that the user has left
       io.to(room).emit("userLeft", { room, name });
+
+      // Step 4: Make the user leave the room
+      socket.leave(room);
     });
 
     socket.on("sendMessage", ({ room, message }) => {
+      // Step 1: Get the user's name from the socketMap
       const { name } = socketMap.get(socket.id);
+
+      // Step 2: Emit a newMessage event to all sockets in the room
       io.to(room).emit("newMessage", { room, name, message });
     });
 
     socket.on("disconnect", () => {
       const { name, roomSet } = socketMap.get(socket.id);
+
+      // Step 1: Remove the user from all rooms in roomSet
       for (let room of roomSet) {
         roomMap.get(room).delete(socket.id);
+      }
+
+      // Step 2: Notify all users in each room that the user has left
+      for (let room of roomSet) {
         io.to(room).emit("userLeft", { room, name });
       }
+
+      // Step 3: Remove the user from socketMap
       socketMap.delete(socket.id);
 
       console.log("User disconnected:", socket.id);
